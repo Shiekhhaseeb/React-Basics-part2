1db6f1f96741c99e28e9b6cee3a7ced6e8bbdbac
Shiekhhaseeb <shiekhhaseeb2019@gmail.com>
2025-07-02 21:34:49 +0530

    How to add css styles(using Inline +Modules css)

--- my-react-app/src/Button.jsx
+++ my-react-app/src/Button.jsx
@@ -1,6 +1,15 @@
 function Button(){
+
+  const styles = {
+    backgroundColor:"hsl(200,100%,50%)" ,
+color: "white",
+padding: "10px 20px",
+bordeRadius: "5px",
+border: "none",
+cursor: "pointer"
+  }
   return(
-<button className="button">CLick Me</button>
+<button style={styles}>CLick Me</button>
   );
 
 }
