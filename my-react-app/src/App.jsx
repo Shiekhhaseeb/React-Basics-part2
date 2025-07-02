a6459bd56e5eb5f3e0cbbe502fec5d60a4388231
Shiekhhaseeb <shiekhhaseeb2019@gmail.com>
2025-07-02 14:20:18 +0530

    card component in react

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,47 @@
+// import  Header from './Header.jsx';
+// import Footer from './Footer.jsx';
+// import Food from './Food.jsx';
+
+// function App() {
+
+//   return(
+//   <>
+//    <Header/>
+//    <Food/>
+   
+//     <Footer/>
+    
+//     </>
+ 
+
+//   );
+// }
+
+// export default App
+/************************** */
+
+import Card from './Card.jsx';
+
+function App() {
+return(
+  <>
+    <Card/>
+        <Card/>
+    <Card/>
+    <Card/>
+
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+
+    <Card/>
+    <Card/>
+
+  </>
+
+)
+
+}
+ 
+export default App
